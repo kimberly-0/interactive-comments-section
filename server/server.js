6beca9634267739199fbd90c781cf6a28882ca1c
Kimberly Dijkmans <kim.14420@outlook.com>
2023-06-23 14:39:01 +0100

    implemented use of cookies to fake user login

--- server/server.js
+++ server/server.js
@@ -1,24 +1,59 @@
-const dotenv = require('dotenv')
-dotenv.config()
+// import { PrismaClient } from "@prisma/client"
 
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser');
-const cors = require('cors');
 
-app.use(bodyParser.json());
+const dotenv = require('dotenv')
+dotenv.config()
+
+const cors = require('cors')
 app.use(cors({
     origin: process.env.CLIENT_URL,
     credentials: true
-}));
+}))
 
-const apiRouter = require('./api/api');
-app.use('/api', apiRouter);
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+const cookieParser = require('cookie-parser')
+app.use(cookieParser({
+    secret: process.env.COOKIE_SECRET
+}))
+
+const { PrismaClient } = require('@prisma/client') 
+const prisma = new PrismaClient()
+
+async function onRequestHook (req, res, next) {
+    const CURRENT_USER_ID = (
+        // Change name to log in as a different person: 'juliusomo', 'maxblagun', 'ramsesmiron', or 'amyrobson'
+        await prisma.user.findFirst({ where: { name: "juliusomo" } }).then(user => {
+            return user.id
+        }).catch(error => {
+            console.log(error)
+        })
+    )
+
+    if (req.cookies.userId !== CURRENT_USER_ID) {
+        req.cookies.userId = CURRENT_USER_ID
+        res.clearCookie("userId")
+        res.cookie("userId", CURRENT_USER_ID)
+    }
+    
+    // console.log("req.cookies : " + req.cookies.userId)
+    // console.log("req.signedCookies : " + req.signedCookies.userId)
+
+    next()
+}
+
+app.use(onRequestHook)
+
+const apiRouter = require('./api/api')
+app.use('/api', apiRouter)  
 
 app.listen({
-    port: process.env.PORT
+    port: process.env.PORT || 3001
 }, () => {
     console.log(`Your server is running on port ${process.env.PORT}`)
 })
 
-module.exports = app;
+module.exports = app
