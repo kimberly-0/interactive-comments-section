6ce3d82db6192196cdb5667c64500de8e260dfc4
Kimberly Dijkmans <kim.14420@outlook.com>
2023-07-05 15:46:36 +0100

    use request instead of cookies for userId

--- client/src/hooks/useUser.js
+++ client/src/hooks/useUser.js
@@ -1,15 +1,15 @@
 import { makeRequest } from "../services/makeRequest";
 
 // get userId of logged-in user through request since cookies are not supported for cross-domain deployment (in case of deployment of client and server on a single domain, use cookies by removing this code and uncommenting the return statement in useUser that uses cookies)
-// const userId = await makeRequest("/getUserId").then(userId => {
-//     return userId
-// }).catch(error => {
-//     console.log(error)
-//     return
-// })
+const userId = await makeRequest("/getUserId").then(userId => {
+    return userId
+}).catch(error => {
+    console.log(error)
+    return
+})
 
 export function useUser() {
-    // return { id: userId }
+    // return { id: document.cookie.match(/userId=(?<id>[^;]+);?$/).groups.id }
 
-    return { id: document.cookie.match(/userId=(?<id>[^;]+);?$/).groups.id }
+    return { id: userId }
 }
